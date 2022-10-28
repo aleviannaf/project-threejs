0dde9b6c832baa715ea8289a21bb5d6d57ff58ac
aleviannaf <alexandrevianna511@gmail.com>
2022-10-28 12:23:16 -0400

    feat(rotation and cub 3): foi usado o conceito de camera, render e cena, além de duas funções de animação

---
+++ scenes/scene-01.js
@@ -0,0 +1,41 @@
+//cria nossa primeira sena
+//precisamos de 3 coisa: geometria, material e mesh
+//geometria é o esqueleto
+//material é a pele
+//mesh é a misntura dos 2
+
+const geometry = new THREE.BoxBufferGeometry();
+const material = new THREE.MeshBasicMaterial({
+    color: 0xa8327f
+});
+
+//nosso mesh
+const cube = new THREE.Mesh(geometry, material);
+
+//add o cubo na cena que foi criado no outro arquivo
+scene.add(cube);
+
+//agora vamos renderizar, para isso vamos passar a cena e a camera
+/// ()   renderer.render(scene, camera)
+
+//agora vamos criar a animação com a função setAnimationLoop
+
+/* renderer.setAnimationLoop(()=>{
+    cube.rotation.x += 0.01;
+    cube.rotation.y +=0.01;
+    
+    camera.rotation.z +=0.01
+    renderer.render(scene, camera);
+   
+    //usar uma outra função mais performatica
+
+}) */
+
+//essa FAZ O MESMO MAS ele para quando troco de tela
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  }
+  animate();
--- setups/setup-01.js
+++ setups/setup-01.js
@@ -1,1 +1,36 @@
-console.log(THREE);
+//Vamos criar a base, o seletro e os tamanhos da nossa cena
+
+const options = {
+  targetSelector: "#scene",
+  with: 800,
+  height: 600,
+  backgroundColor: 0x3498eb
+};
+
+//vamos criar nosso renderizador, onde vamos usar o webgl para renderizar
+const renderer = new THREE.WebGLRenderer();
+
+//criado o renderizador, vamos dar um tamanho para ele
+renderer.setSize(options.with, options.height);
+
+//agora vamos pegar nosso seletor e add o nosso renderizador como filho
+document.querySelector(options.targetSelector).appendChild(renderer.domElement);
+
+//criar nossa cena
+const scene = new THREE.Scene();
+
+//para adicionar uma cor
+scene.background = new THREE.Color(
+  options.backgroundColor
+);
+
+// agora vou criar a camera
+//vou usar a perspective camera pois ele simula a visão humana
+
+const camera = new THREE.PerspectiveCamera(
+    50,
+    options.with/ options.height,
+);
+
+//posição da camera
+camera.position.z = 5;
